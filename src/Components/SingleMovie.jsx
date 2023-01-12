c99a447b5fd0da0ae6149cf979a4d2c75939b1ed
panchu lal saini <sainip103@gmail.com>
2023-01-12 16:27:25 +0530

    updated my reposetry

--- src/Components/SingleMovie.jsx
+++ src/Components/SingleMovie.jsx
@@ -57,7 +57,7 @@ function SingleMovie() {
         </div>
       </div>
       <div>
-        <button className = 'SM-btn'><Link to="/">GoTo Home</Link> </button>
+        <button className = 'SM-btn'><Link to='/ImdbMovies'>GoTo Home</Link> </button>
       </div>
     </>
   )
